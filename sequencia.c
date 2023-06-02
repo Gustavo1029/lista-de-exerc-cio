1807b69dbb7dfc85b36a740e92dae17eb11d2c79
Gustavo1029 <135294193+Gustavo1029@users.noreply.github.com>
2023-06-02 00:45:13 -0300

    sequência.c

---
+++ sequencia.c
@@ -0,0 +1,17 @@
+/******************************************************************************
+
+Gustavo Nogueira Borges
+15/02/2023
+Escreva um algoritmo que imprima as seguintes sequencias
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    int n = 1;
+    
+    while (n <= 10){
+        printf("%d, (12345678910)\n", n++);
+    }
+}
